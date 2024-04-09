d03ec2b6494c3ea7c2ddc238e2563bb4340009d4
logonrmlocal <otavicoelho498@gmail.com>
2024-04-08 22:40:14 -0300

    aula04.py

--- aula04.py
+++ aula04.py
@@ -8,24 +8,50 @@
 #print(x)
 #x = 3
 #print(x)
-#
+# ----------------------------------------------------------
 #x = 1
 #print(x)
 #x = x + 1
 #print(x)
 #x = x + 1
 #print(x)
-#
-#x = 1
-#while x <= 100:
+# ----------------------------------------------------------
+# x = 0
+# while x <= 99:
 #    x += 1
-#    print("")
-
+#    print(x)
+# ----------------------------------------------------------
+#from time import sleep
+# ----------------------------------------------------------
+#x = 10
+#while x >= 0:
+#    print(x)
+#    sleep(3600)
+#    x -= 1
+# ----------------------------------------------------------
+#print("FOGO!")
 from time import sleep
-
-x = 10
-while x >= 0:
-    print(x)
-    sleep(3600)
-    x -= 1
-print("FOGO!")
+#fim = int(input("Digite um numero = "))
+#x = 0
+#while x <= fim:
+#    print(x)
+#    sleep(1)
+#    x += 1
+# ----------------------------------------------------------
+# fim = int(input("Digite um numero = "))
+# x = 1
+# while x <= fim:
+#    print(x)
+#    x += 2
+# ----------------------------------------------------------
+# fim = int(input("digite um numero = "))
+# x = 0
+# while x <= fim:
+#    print(x)
+#    x += 3
+# ----------------------------------------------------------
+# num = int(input(" Manda a tabuada pra nois = "))
+# x = 0
+# while x <= 10:
+#     print(f"{num} x {x} = {x * num}")
+#     x += 1
