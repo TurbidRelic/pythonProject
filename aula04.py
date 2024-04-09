85679c8888e10b8d7bfc4239bd15ddd1ba4d9c39
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-08 21:58:17 -0300

    Initial commit

---
+++ aula04.py
@@ -0,0 +1,31 @@
+#print(1)
+#print(2)
+#print(3)
+
+#x = 1
+#print(x)
+#x = 2
+#print(x)
+#x = 3
+#print(x)
+#
+#x = 1
+#print(x)
+#x = x + 1
+#print(x)
+#x = x + 1
+#print(x)
+#
+#x = 1
+#while x <= 100:
+#    x += 1
+#    print("")
+
+from time import sleep
+
+x = 10
+while x >= 0:
+    print(x)
+    sleep(3600)
+    x -= 1
+print("FOGO!")
